5c174cd9feee26f6976e17a1f53e25d83b040198
Juan Veronez <juan.monteirov@gmail.com>
2025-06-20 15:16:01 -0300

    feat: add cva and create text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Text from "./components/text";
+
 export default function App() {
   return (
-    <h1 className="text-green-dark">Hello World!</h1>
+    <>
+      <Text as="h1" variant="body-md-bold">Hello World!</Text>
+      <Text variant="body-sm-bold">Hello World!</Text>
+      <Text as="p" className="text-pink-base">Hello World!</Text>
+    </>
   )
 }
